2819d1df194c808ffa78b80c5640c4fe33a067cc
Dilawar Singh <dilawar@dognosis.tech>
2025-03-23 17:09:47 +0530

    Recording can be started/stopped easily

--- leptos/src/components/audio.rs
+++ leptos/src/components/audio.rs
@@ -1,21 +1,57 @@
 //! Audio Streamer component
 
 use leptos::prelude::*;
-use leptos_qr_scanner::Scan;
-use reactive_stores::Store;
+use leptos_use::{use_user_media_with_options, UseUserMediaOptions, UseUserMediaReturn};
 use thaw::*;
 
 use crate::css::styles;
-use crate::storage::{GlobalState, GlobalStateStoreFields};
 
 #[component]
 pub fn AudioStream() -> impl IntoView {
-    let stream = RwSignal::new(true);
+    let node = NodeRef::<leptos::html::Audio>::new();
 
-    let result = RwSignal::new(vec![]);
+    let start_rec = RwSignal::new(true);
+    let result = RwSignal::new_local("".to_string());
+
+    let options = UseUserMediaOptions::default().video(false).audio(true);
+
+    let UseUserMediaReturn {
+        stream,
+        start,
+        stop,
+        ..
+    } = use_user_media_with_options(options);
+
+    start();
+
+    let effect = Effect::watch(
+        move || start_rec.get(),
+        move |val, _prev, _| {
+            if !val {
+                tracing::info!("Stopping recording.");
+                stop();
+            } else {
+                tracing::info!("Starting recording.");
+                start();
+            }
+        },
+        false,
+    );
+
+    Effect::new(move |_| {
+        tracing::info!("State of the recording: {}.", start_rec.get_untracked());
+        node.get().map(|v| match stream.get() {
+            Some(Ok(s)) => v.set_src_object(Some(&s)),
+            Some(Err(e)) => tracing::error!("Failed to get media stream: {e:?}"),
+            None => tracing::debug!("No stream yet"),
+        });
+    });
 
     view! {
-
-        <Checkbox checked=stream label="Record" />
+        <Space vertical=true>
+            <audio node_ref=node controls  />
+            <Checkbox checked=start_rec label="Start Record" />
+            <div> { start_rec } </div>
+        </Space>
     }
 }
