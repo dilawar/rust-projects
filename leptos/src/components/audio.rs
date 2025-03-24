1a29a45fceb6fbf419323648e1c3347a3eb0105d
Dilawar Singh <dilawar@dognosis.tech>
2025-03-24 22:31:40 +0530

    Plot raw data

--- leptos/src/components/audio.rs
+++ leptos/src/components/audio.rs
@@ -15,8 +15,9 @@ use crate::css::styles;
 #[component]
 pub fn AudioStream() -> impl IntoView {
     let node = NodeRef::<leptos::html::Audio>::new();
+    let canvas_node = NodeRef::<leptos::html::Canvas>::new();
 
-    let start_rec = RwSignal::new(true);
+    let start_rec = RwSignal::new(false);
     let async_blob = RwSignal::new(None);
 
     // Create options to fetch only audio stream.
@@ -50,7 +51,7 @@ pub fn AudioStream() -> impl IntoView {
                 v.set_src_object(Some(&s));
                 let recorder = MediaRecorder::new_with_media_stream(&s).unwrap();
                 recorder.set_ondataavailable(Some(on_data_available.as_ref().unchecked_ref()));
-                recorder.start_with_time_slice(5000).unwrap();
+                recorder.start_with_time_slice(500).unwrap();
             }
             Some(Err(e)) => tracing::error!("Failed to get media stream: {e:?}"),
             None => tracing::debug!("No stream yet"),
@@ -74,9 +75,27 @@ pub fn AudioStream() -> impl IntoView {
 
     // Watch async_blob
     Effect::new(move |_| {
+        let ctx = canvas_node
+            .get()
+            .unwrap()
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+
         if let Some(blob_resource) = async_blob.get() {
             if let Some(Ok(data)) = blob_resource.read().as_deref() {
                 tracing::info!("Got blob of buffer {data:?}");
+                ctx.reset();
+                ctx.begin_path();
+                for (i, v) in data.iter().enumerate() {
+                    ctx.line_to(
+                        i as f64,
+                        *v as f64 / 10.0 + 50.0, /* 50 is half of height of canvas */
+                    )
+                }
+                ctx.stroke(); // render
             }
         }
     });
@@ -84,9 +103,12 @@ pub fn AudioStream() -> impl IntoView {
     view! {
         <Space vertical=true>
             // Eventually I was to draw something related to audio stream here.
-            <canvas />
+            <canvas node_ref=canvas_node class=styles::canvas  />
             <audio node_ref=node controls />
             <Switch checked=start_rec label="Start Record" />
+            <div>
+                "Record and plot every half second of data"
+            </div>
         </Space>
     }
 }
